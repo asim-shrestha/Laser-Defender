7248ffcaefe08ec40416264a7e29c99f302bbad3
Asim Shrestha <asim.shrestha@hotmail.com>
2019-05-01 21:28:16 -0700

    waypoint work

--- Laser Defender/Assets/Classes/Enemy.cs
+++ Laser Defender/Assets/Classes/Enemy.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	[SerializeField] int health = 3;
+	[SerializeField] float enemySpeed = 5f;
+	[SerializeField] List<Transform> wayPoints;
+	private int wayPointIndex = 0;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		//Start at the first waypoint
+		transform.position = wayPoints[wayPointIndex].position;
+		wayPointIndex++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		//Check if we have reached the end of the path
+		if (wayPointIndex <= wayPoints.Count - 1) {
+			//Move towards next waypoint
+			var targetPosition = wayPoints[wayPointIndex].position;
+			var movementhThisFrame = enemySpeed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementhThisFrame);
+
+			//Check if waypoint reached
+			if(transform.position == targetPosition) {
+				wayPointIndex++;
+			}
+		}
+
+		else {
+			Destroy(this.gameObject);
+		}
+
     }
 
 	private void OnTriggerEnter2D(Collider2D collision) {
